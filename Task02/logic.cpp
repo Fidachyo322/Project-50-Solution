50bea2fe590414db99bbb06286133611e93915be
Fidachyo322 <baconvevo@mail.ru>
2025-04-18 19:39:16 +0300

    Task 02

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -61,23 +61,25 @@ int get_extreme_elements_up_main_diagonale(int matrix[SIZE][SIZE], int size) {
 //	return sum;
 //}
 
-int sum_elements_down_main_diagonale(int matrix[SIZE][SIZE], int size) {
+int count_even_elements_down_main_diagonale(int matrix[SIZE][SIZE], int size) {
 	if (size <= 0 || !matrix)
 	{
 		return 0;
 	}
 
-	int sum = 0;
+	int count = 0;
 
 	for (int i = 1; i < size; i++)
 	{
 		for (int j = 0; j < i; j++)
 		{
-			sum += matrix[i][j];
+			if (matrix[i][j] % 2 == 0) {
+				count++;
+			}
 		}
 	}
 
-	return sum;
+	return count;
 }
 
 int sum_elements_up_additional_diagonale(int matrix[SIZE][SIZE], int size) {
--- Task02/main.cpp
+++ Task02/main.cpp
@@ -13,6 +13,11 @@ int main() {
 	cout << "Max element up main diagonale is "
 		<< get_extreme_elements_up_main_diagonale(matrix, size) << endl;
 
+
+	cout << "Count even element down main diagonale is "
+		<< count_even_elements_down_main_diagonale(matrix, size) << endl;
+
+	
 	system("pause");
 		
 	return 0;
