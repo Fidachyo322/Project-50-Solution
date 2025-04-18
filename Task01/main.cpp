10693181a3a536fab8c9184b59c292ad8b956bf9
Fidachyo322 <baconvevo@mail.ru>
2025-04-18 18:58:50 +0300

    all Tasks

--- Task01/main.cpp
+++ Task01/main.cpp
@@ -11,10 +11,16 @@ int main() {
 	cout << "Matrix:\n" << convert(matrix, size);
 
 	cout << "Sum matrix elements up main diagonale: "
-		 << sum_elements_up_main_deagonale(matrix, size) << endl;
+		 << sum_elements_up_main_diagonale(matrix, size) << endl;
 
 	cout << "Sum matrix elements down main diagonale: "
-		<< sum_elements_down_main_deagonale(matrix, size) << endl;
+		<< sum_elements_down_main_diagonale(matrix, size) << endl;
+
+	cout << "Sum matrix elements up additional diagonale: "
+		<< sum_elements_up_additional_diagonale(matrix, size) << endl;
+
+	cout << "Sum matrix elements down additional diagonale: "
+		<< sum_elements_down_additional_diagonale(matrix, size) << endl;
 
 	system("pause");
 		
